27512131ceceb58564e614a6911518790d6479d2
Султан <carameleclipsimus@gmail.com>
2023-06-09 11:30:39 +0600

    feat: пока не решено

--- script.js
+++ script.js
@@ -13,3 +13,30 @@ function timeToMixJuice(name) {
       return 2.5;
   }
 }
+
+function limesToCut(wedgesNeeded, limes) {
+  let count = 0;
+  for (let i = 0; i < limes.length; i++) {
+    switch (limes[i]) {
+      case "small":
+        limes[i] = 6;
+        break;
+      case "medium":
+        limes[i] = 8;
+        break;
+      case "large":
+        limes[i] = 10;
+        break;
+    }
+    if (wedgesNeeded >= 0) {
+      count++;
+      wedgesNeeded -= limes[i];
+      if (wedgesNeeded - limes[i] < 0) {
+        break;
+      }
+    }
+  }
+  return count;
+}
+
+console.log(limesToCut(25, ["small", "small", "large", "medium", "small"]));
