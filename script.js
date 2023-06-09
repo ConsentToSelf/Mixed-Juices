c69e0a018230f62a5be36ba76edf7c03429b5ee2
Султан <carameleclipsimus@gmail.com>
2023-06-09 20:13:23 +0600

    feat: done

--- script.js
+++ script.js
@@ -14,29 +14,47 @@ function timeToMixJuice(name) {
   }
 }
 
-function limesToCut(wedgesNeeded, limes) {
-  let count = 0;
-  for (let i = 0; i < limes.length; i++) {
-    switch (limes[i]) {
-      case "small":
-        limes[i] = 6;
-        break;
-      case "medium":
-        limes[i] = 8;
-        break;
-      case "large":
-        limes[i] = 10;
-        break;
+// function limesToCut(wedgesNeeded, limes) {
+//   let count = 0;
+//   for (let i = 0; i < limes.length; i++) {
+//     switch (limes[i]) {
+//       case "small":
+//         limes[i] = 6;
+//         break;
+//       case "medium":
+//         limes[i] = 8;
+//         break;
+//       case "large":
+//         limes[i] = 10;
+//         break;
+//     }
+//     if (wedgesNeeded > 0) {
+//       count++;
+//       wedgesNeeded -= limes[i];
+//     }
+//     if (wedgesNeeded === 0) return 0;
+//   }
+//   return count;
+// }
+
+// console.log(limesToCut(0, ["small", "small", "large", "medium", "small"]));
+
+function remainingOrders(timeLeft, orders) {
+  let arr = [];
+  for (let i = 0; i < orders.length; i++) {
+    if (timeLeft <= 0) {
+      arr.push(orders[i]);
     }
-    if (wedgesNeeded > 0) {
-      count++;
-      wedgesNeeded -= limes[i];
-    }
-    if (wedgesNeeded === 0) return 0;
+    timeLeft -= timeToMixJuice(orders[i]);
   }
-  return count;
+  return arr;
 }
-
-console.log(limesToCut(0, ["small", "small", "large", "medium", "small"]));
-
-// 25 - 6 = 19, 19 - 6 = 13, 13 - 10 = 3,
+console.log(
+  remainingOrders(7, [
+    "Tropical Island",
+    "Energizer",
+    "Limetime",
+    "All or Nothing",
+    "Pure Strawberry Joy",
+  ])
+);
